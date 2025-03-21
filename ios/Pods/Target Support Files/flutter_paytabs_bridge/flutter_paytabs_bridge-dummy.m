4f553514c11bff7f89416fb894466ca19447ab0f
Faiz57 <faiz@cypruspremier.com>
2025-03-21 23:08:17 +0300

    Office-PC

--- ios/Pods/Target Support Files/flutter_paytabs_bridge/flutter_paytabs_bridge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_paytabs_bridge : NSObject
-@end
-@implementation PodsDummy_flutter_paytabs_bridge
-@end
--- ios/Pods/Target Support Files/paytm/paytm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_paytm : NSObject
-@end
-@implementation PodsDummy_paytm
-@end
